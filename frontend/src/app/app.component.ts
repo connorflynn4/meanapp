c50ef5e03fbbb6a6a80b01ea27d9bc72e24bf572
Connor Flynn <connorflynn4@gmail.com>
2019-01-09 15:52:05 +0000

    frontend complete with bg image

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+
+  getUrl() {
+      return "url('https://cdn.japantimes.2xx.jp/wp-content/uploads/2017/12/p7-debito-a-20180104-870x522.jpg')";
+    }
 }
